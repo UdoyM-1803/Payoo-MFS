0b63e49306ab887d8464bd4ecff0d082639e57db
UdoyM-1803 <udoymunna17@gmail.com>
2025-08-28 19:57:59 +0600

    Added pin number and navigation to Home page

--- js/login.js
+++ js/login.js
@@ -1,14 +1,44 @@
-// console.log('button clicking file added.')
+// // console.log('button clicking file added.')
 
-// document.getElementById('id').addEventListener('event(click/mouseover)', function(){})
+// // document.getElementById('id').addEventListener('event(click/mouseover)', function(){})
 
-// Step-1: Set Event Handler************************
-document.getElementById('btn-login').addEventListener('click', function(event){
-    // Step-2: Prevent default behavior (Prevent Reloading Browser)*************************************
-    event.preventDefault();  // Little bit complicated.
-    console.log('Login Button Clicked.');
+// // Step-1: Set Event Handler************************
+// document.getElementById('btn-login').addEventListener('click', function(event){
+//     // Step-2: Prevent default behavior (Prevent Reloading Browser)*************************************
+//     event.preventDefault();  // Little bit complicated.
 
-    // Step-3: Get the Phone Number******************
+//     // Step-3: Get the Phone Number and the Pin Number******************
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+
+//     console.log(phoneNumber, pinNumber);
+
+//     Validate the Phone Number and Pin Number...
+//     This is Temporary. It is not Advisable........
+//     if (phoneNumber === '5' && pinNumber === '1234') {
+//         console.log('You are logged in');
+//         // Step-5: Allow user to use the website.
+//     }
+//     else {
+//         alert('Wrong phone number or Pin');
+//     }
+// })
+
+document.getElementById('btn-login').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // Get phone number and pin
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log(phoneNumber, pinNumber);
+
+    // Bad way to validate
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('You are logged in');
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('Wrong phone Number or Pin Number');
+    }
 })
