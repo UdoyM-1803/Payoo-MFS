45769cf8c0fb14dfe8b5f023446074b6e4d73fde
UdoyM-1803 <udoymunna17@gmail.com>
2025-08-29 19:51:26 +0600

    Added Cash Out features to the UI/DOM and hide the addmoney features.

---
+++ js/cashout.js
@@ -0,0 +1,22 @@
+document.getElementById('btn-cash-out')
+    .addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const cashOut = document.getElementById('input-cash-out').value;
+        const cashOutNumber = parseFloat(cashOut);
+        const pinNumber2 = document.getElementById('input-cash-out-pin').value;
+
+        console.log(cashOut, pinNumber2);
+
+        if (pinNumber2 === '1234') {
+            const balance = document.getElementById('account-balance').innerText;
+            const balanceNumber = parseFloat(balance);
+
+            const newBalance = balanceNumber - cashOutNumber;
+
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else {
+            alert('Failed to Cash Out! Please, try again later.')
+        }
+    })
